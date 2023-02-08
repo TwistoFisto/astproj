7729d6038baa0587abe01af32400c07df69b51f5
TwistoFisto <49349674+TwistoFisto@users.noreply.github.com>
2023-02-08 17:52:48 +0000

    idk what going on anymore kek

--- burstLineCalPotencyOnly.py
+++ burstLineCalPotencyOnly.py
@@ -1,3 +1,4 @@
+from random import triangular
 import pandas as pd
 import math
 
@@ -6,15 +7,19 @@ lvlModifier_Div = 1900
 lvlModifier_Main = 390
 
 print("Please input your stats")
-job = "SAM"#input("Class: ")
+job = "BRD"#input("Class: ")
 crit = 2000#int(input("Crit:"))
 dhit = 1500#int(input("Direct Hit:"))
 speed = 1473 #int(input("Skill Speed/Spellspeed:"))
 
-dataframe = pd.read_csv('justframecards-BasePotencies.csv')
-dataframe["Movename"] = dataframe["Movename"].str.lower()
+movenamelist = pd.read_csv('justframecards-BasePotencies.csv')
+movenamelist["Movename"] = movenamelist["Movename"].str.lower()
 
-job_moves = dataframe.query("Class == @job")
+traitBuffList = pd.read_csv('justframecards-TraitsAndBuffs.csv')
+traitBuffList['Buff'] = traitBuffList['Buff'].str.rstrip('%').astype('float') / 100.0
+print(traitBuffList.to_string())
+
+job_moves = movenamelist.query("Class == @job")
 a = len(job_moves.index)
 print(a)
 print(job_moves.to_string())
@@ -33,7 +38,7 @@ burstline = []
 GCD_Count  = math.floor(GCD_Count)
 oGCD_weave_count = 0
 
-
+"""
 #WE'RE GOING TO NEED TO ADD CAST TIMES COLUMN SO WE KNOW IF IT'S: NO WEAVE, SINGLE WEAVE ONLY OR FREE DOUBLE WEAVE
 for i in range(GCD_Count):
     print("debug mesg")
@@ -86,9 +91,108 @@ for i in range(GCD_Count):
                 burstline.append(ability_name3)
                 pass
             
-    print("debug1",burstline)
+    print("debug1",burstline)"""
+
+
+#Getting baseline potency including buffs - also my python wasn't updated so we're nesting fucking if statements yippee:
+
+if job == "SAM":
+    SAMBuffList = traitBuffList.query("Class == @job")
+    Jinpu = SAMBuffList.query("Name == 'Jinpu'").iloc[0,2]
+    print("Jinpu", Jinpu)
+    Shifu = SAMBuffList.query("Name == 'Shifu'").iloc[0,2]  
+    pass
+
+elif job == "NIN":
+    NINBuffList = traitBuffList.query("Class == @job")
+    Huton = NINBuffList.query("Name == 'Huton'").iloc[0,2]
+    Trick_Attack = NINBuffList.query("Name == 'Trick Attack'").iloc[0,2]
+    Mug = NINBuffList.query("Name == 'Mug'").iloc[0,2]
+    pass
+
+elif job == "DRG":
+    DRGBuffList = traitBuffList.query("Class == @job")
+    Battle_Litany = DRGBuffList.query("Name == 'Battle Litany'").iloc[0,2]
+    Dragonsight_Self = DRGBuffList.query("Name == 'Dragonsight Self'").iloc[0,2]
+    Disembowl = DRGBuffList.query("Name == 'Disembowl'").iloc[0,2]
+    Lance_Charge = DRGBuffList.query("Name == 'Lance Charge'").iloc[0,2]
+    pass
+
+elif job == "MNK":
+    MNKBuffList = traitBuffList.query("Class == @job")
+    Disciplined_Fists = MNKBuffList.query("Name == 'Disciplined Fists'").iloc[0,2]
+    Greased_Lightning = MNKBuffList.query("Name == 'Greased Lightning'").iloc[0,2]
+    Riddle_of_Fire = MNKBuffList.query("Name == 'Riddle of Fire'").iloc[0,2]
+    Riddle_of_Wind = MNKBuffList.query("Name == 'Riddle of Wind'").iloc[0,2]
+    Brotherhood = MNKBuffList.query("Name == 'Brotherhood'").iloc[0,2]
+    pass
+
+elif job == "RPR":
+    RPRBuffList = traitBuffList.query("Class == @job")
+    Shadow_of_Death = RPRBuffList.query("Name == 'Shadow of death'").iloc[0,2]
+    Arcane_Circle = RPRBuffList.query("Name == 'Arcane Circle'").iloc[0,2]
+    pass
+
+elif job == "BRD":
+    BRDBuffList = traitBuffList.query("Class == @job | Class == 'ALL PHY RANGED'")
+    #print(BRDBuffList.to_string())
+    Increased_Action_DMG = BRDBuffList.query("Name == 'Increased Action DMG'").iloc[0,2]
+    Raging_Strikes = BRDBuffList.query("Name == 'Raging Strikes'").iloc[0,2]
+    Battle_Voice = BRDBuffList.query("Name == 'Battle Voice'").iloc[0,2]
+    Mages_Ballad = BRDBuffList.query("Name == 'Mages Ballad'").iloc[0,2]
+    Wanderers_Minuet = BRDBuffList.query("Name == 'Wanderers Minuet'").iloc[0,2]
+    Armys_Paeon = BRDBuffList.query("Name == 'Armys Paeon'").iloc[0,2]
+    Radiant_Finale = BRDBuffList.query("Name == 'Radiant Finale'").iloc[0,2]
+    BRDBuffArray = [Increased_Action_DMG,Raging_Strikes,Battle_Voice,Mages_Ballad,Wanderers_Minuet,Armys_Paeon,Radiant_Finale]
+    print(BRDBuffArray)
+    pass
+
+elif job == "MCH":
+    pass
+elif job == "DNC":
+    pass
+elif job == "BLM":
+    pass
+elif job == "SMN":
+    pass
+elif job == "RDM":
+    pass
+elif job == "WAR":
+    pass
+elif job == "PLD":
+    pass
+elif job == "DRK":
+    pass
+elif job == "GNB":
+    pass
+elif job == "SGE":
+    pass
+elif job == "SCH":
+    pass
+elif job == "AST":
+    pass
 
 """
+match job:
+    case "SAM":
+        buffs = job_moves = traitBuffList.query("Class == @job")
+    case "NIN":
+    case "DRG":
+    case "MNK":
+    case "RPR":
+    case "BRD":                
+    case "MCH":
+    case "DNC":
+    case "BLM":
+    case "SMN":
+    case "RDM":
+    case "WAR":
+    case "PLD":
+    case "DRK":
+    case "GNB":     
+"""
+"""    
+
 SAM1 = SAM.query("Movename == 'midare setsugekka'")
 print(SAM1.to_string())
 
